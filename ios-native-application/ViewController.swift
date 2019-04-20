0dfa21be6ae64d76ffc5d6ce8839f03909bd0524
Evgenii Khramkov <evkhramk@microsoft.com>
2019-04-20 18:43:08 +0300

    Initial Commit

---
+++ ios-native-application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-native-application
+//
+//  Created by Evgenii Khramkov on 4/20/19.
+//  Copyright © 2019 ewgeniux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
