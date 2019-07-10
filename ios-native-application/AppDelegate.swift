5ade75b46204d34c1970855416b8bb042d861dda
Yunlei Liu <yunll@microsoft.com>
2019-07-09 17:10:53 -0700

    add appcenter sdk

--- ios-native-application/AppDelegate.swift
+++ ios-native-application/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CoreData
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("7ab2783d-445c-4520-bd0f-d91f64c0bd2d", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         return true
     }
 
